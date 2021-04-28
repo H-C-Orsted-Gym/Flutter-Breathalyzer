b3b26b6c76a667881c47b13b5b67e62d9f9ad9aa
fredrikj31 <fredrik1206@gmail.com>
2021-04-28 20:44:28 +0200

    Added tracking component

---
+++ lib/components/TrackingComponent.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrackingComponent extends StatelessWidget {
+  int tracking;
+  DateTime dateTracked;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
